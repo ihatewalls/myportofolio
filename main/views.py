32f9097a7c98097df24023b075064b5ca58ac561
ihatewalls <musthofa.ridho@ui.ac.id>
2026-09-09 17:18:40 +0700

    feat: Create Skills Page and Allow Accessing

--- main/views.py
+++ main/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-from main.models import Experience
+from main.models import Experience, Skill
 
 
 def show_main(request):
@@ -21,3 +21,10 @@ def show_experience(request):
         "experience_list": Experience.objects.all(),
     }
     return render(request, "experience.html", context)
+
+def show_skill(request):
+    context = {
+        "name": "Ridho",
+        "skill_list": Skill.objects.all(),
+    }
+    return render(request, "skill.html", context)
