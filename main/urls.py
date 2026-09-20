64807f9a5a519ab00ceae30f2ae08886212a2bb0
ihatewalls <musthofa.ridho@ui.ac.id>
2026-09-20 14:03:31 +0700

    feat: Creating edit function and path for experiences

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from main.views import show_main, show_experience, show_skill, create_skill, get_skills_json, delete_skill, create_experience,get_experience_json, delete_experience
+from main.views import edit_experience, show_main, show_experience, show_skill, create_skill, get_skills_json, delete_skill, create_experience,get_experience_json, delete_experience, edit_experience
 
 app_name = "main"
 
@@ -14,4 +14,5 @@ urlpatterns = [
     path("experience/add/", create_experience, name="create_experience"),
     path("api/experience/", get_experience_json, name="get_experience_json"),
     path("experience/<uuid:experience_id>/delete/", delete_experience, name="delete_experience"),
+    path("experience/<uuid:experience_id>/edit/", edit_experience, name="edit_experience"),
 ]
--- main/views.py
+++ main/views.py
@@ -1,3 +1,5 @@
+from math import exp
+
 from django.contrib import messages
 from django.core import serializers
 from django.http import HttpResponse
@@ -87,7 +89,7 @@ def delete_skill(request, skill_id):
 
 def create_experience(request):
     form = ExperienceForm(request.POST or None)
-
+    
     if request.method == "POST" and form.is_valid():
         form.save()
         messages.success(request, "Experience baru berhasil ditambahkan!")
@@ -118,3 +120,19 @@ def delete_experience(request, experience_id):
         return redirect("main:show_experience")
 
     return redirect("main:show_experience")
+
+def edit_experience(request, experience_id):
+    experience = get_object_or_404(Experience, pk=experience_id)
+    form = ExperienceForm(request.POST or None, instance=experience)
+    
+    if request.method == "POST" and form.is_valid():
+        form.save()
+        messages.success(request, "Experience baru berhasil ditambahkan!")
+        return redirect("main:show_experience")
+
+    context = {
+        "name": "Ridho",
+        "form": form,
+        "experience": experience,
+    }
+    return render(request, "experience_edit_form.html", context)
