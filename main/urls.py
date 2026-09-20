b0942ddb591dfc50764ebe74fa4dfb8a96442913
ihatewalls <musthofa.ridho@ui.ac.id>
2026-09-20 10:51:02 +0700

    feat: added url path to add experiences

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from main.views import show_main, show_experience, show_skill, create_skill, get_skills_json, delete_skill
+from main.views import show_main, show_experience, show_skill, create_skill, get_skills_json, delete_skill, create_experience
 
 app_name = "main"
 
@@ -10,5 +10,6 @@ urlpatterns = [
     path("skills/", show_skill, name="show_skill"),
     path("skills/add/", create_skill, name="create_skill"),
     path("api/skills/", get_skills_json, name="get_skills_json"),
-    path("skills/<uuid:skill_id>/delete/", delete_skill, name="delete_skill")
+    path("skills/<uuid:skill_id>/delete/", delete_skill, name="delete_skill"),
+    path("experience/add/", create_experience, name="create_experience"),
 ]
