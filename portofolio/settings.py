0be73d2eeedfe2346b177f98e9237a711815dafb
ihatewalls <musthofa.ridho@ui.ac.id>
2026-09-20 11:12:32 +0700

    fix: CSRF token including a trailing /

--- portofolio/settings.py
+++ portofolio/settings.py
@@ -151,4 +151,4 @@ MAILERS = {
     },
 }
 
-CSRF_TRUSTED_ORIGINS = ["https://musthofa-ridho-myportofolio.pws.cs.ui.ac.id/"]
+CSRF_TRUSTED_ORIGINS = ["https://musthofa-ridho-myportofolio.pws.cs.ui.ac.id"]
