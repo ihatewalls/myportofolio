54fd8c90903fac6408ccf42d6111cb757051311a
ihatewalls <musthofa.ridho@ui.ac.id>
2026-09-21 15:14:35 +0700

    feat: Dark mode now persists when switching pages

--- static/js/dark_mode.js
+++ static/js/dark_mode.js
@@ -1,4 +1,13 @@
 const darkmode = document.getElementById("darkmode");
 darkmode.addEventListener("change", function() {
     document.body.classList.toggle("dark-mode", darkmode.checked);
+    if (document.body.classList.contains("dark-mode")){
+        localStorage.setItem("theme", "dark")
+    }else{
+        localStorage.setItem("theme", "light")
+    }
 });
+if(localStorage.getItem("theme")==="dark"){
+    darkmode.checked = true
+    document.body.classList.toggle("dark-mode", darkmode.checked);
+}
